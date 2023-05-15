ae1c4cedc979467c34882e6fc4864b37fdbf1e44
G3Kappa <gkevin@protonmail.com>
2023-05-15 04:08:38 +0200

    wip

--- Fiero.Business/Fiero.Business/BUS.Structures/UI/Modals/ContainerModal.cs
+++ Fiero.Business/Fiero.Business/BUS.Structures/UI/Modals/ContainerModal.cs
@@ -91,7 +91,7 @@ namespace Fiero.Business
                 .Apply(x =>
                 {
                     x.CenterContentH.V = false;
-                    x.FontSize.V = 8;
+                    x.FontSize.V = 18;
                     x.Padding.V = new(16, 0);
                 }))
             ;
@@ -102,13 +102,13 @@ namespace Fiero.Business
             return layout
                 .Repeat(PageSize.V, (index, grid) => grid
                 .Row(@class: index % 2 == 0 ? "row-even" : "row-odd")
-                    .Col(w: 32, px: true, @class: "item-sprite")
+                    .Col(w: 0.08f, @class: "item-sprite")
                         .Cell<Picture>(p =>
                         {
                             Invalidated += () => RefreshItemSprite(p, index);
                         })
                     .End()
-                    .Col(w: 1f, @class: "item-name")
+                    .Col(w: 1.94f, @class: "item-name")
                         .Cell<Button>(b =>
                         {
                             Invalidated += () => RefreshItemButton(b, index);
@@ -117,7 +117,7 @@ namespace Fiero.Business
                     .End()
                 .End())
                 .Row()
-                    .Col(w: 1f, px: false, @class: "paginator paginator-prev")
+                    .Col(w: 0.25f, @class: "paginator paginator-prev")
                         .Cell<Button>(b =>
                         {
                             b.Text.V = "<";
@@ -128,13 +128,13 @@ namespace Fiero.Business
                             };
                         })
                     .End()
-                    .Col(w: 1f, @class: "paginator paginator-current")
+                    .Col(w: 2.50f, @class: "paginator paginator-current")
                         .Cell<Label>(l =>
                         {
                             Invalidated += () => RefreshPageLabel(l);
                         })
                     .End()
-                    .Col(w: 1f, px: false, @class: "paginator paginator-next")
+                    .Col(w: 0.25f, @class: "paginator paginator-next")
                         .Cell<Button>(b =>
                         {
                             b.Text.V = ">";

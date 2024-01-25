b14e2b323e1762cd72af38ada12a3dfa8bd5f7b0
G3Kappa <gkevin@protonmail.com>
2024-01-25 17:01:28 +0100

    GRAPPLING HOOK

--- Fiero.Business/Fiero.Business/BUS.Services/Scripting/Ergo/Built-Ins/TriggerEffect.cs
+++ Fiero.Business/Fiero.Business/BUS.Services/Scripting/Ergo/Built-Ins/TriggerEffect.cs
@@ -9,8 +9,8 @@ namespace Fiero.Business;
 [SingletonDependency]
 public sealed class TriggerEffect(IServiceFactory services) : BuiltIn("", new("effect"), 3, FieroLib.Modules.Effect)
 {
-    [Term(Functor = "effect_def", Marshalling = TermMarshalling.Positional)]
-    internal readonly record struct EffectDefStub(EffectName Name, string Arguments);
+    [Term(Functor = "effect_def", Marshalling = TermMarshalling.Named)]
+    internal readonly record struct EffectDefStub(EffectName Name, string Arguments, string Duration, string Chance, string CanStack);
 
     private readonly IServiceFactory _services = services;
     public override ErgoVM.Op Compile()
@@ -22,9 +22,13 @@ public sealed class TriggerEffect(IServiceFactory services) : BuiltIn("", new("e
             var args = vm.Args;
             if (args[0].Matches<EffectDefStub>(out var stub))
             {
+                int? duration = int.TryParse(stub.Duration, out var d) ? d : null;
+                float? chance = float.TryParse(stub.Chance, out var c) ? c : null;
+                bool canStack = bool.TryParse(stub.CanStack, out var b) ? b : false;
+
                 if (args[1].IsEntity<Entity>().TryGetValue(out var e))
                 {
-                    var def = new EffectDef(stub.Name, stub.Arguments, source: e);
+                    var def = new EffectDef(stub.Name, stub.Arguments, chance: chance, duration: duration, canStack: canStack, source: e);
                     var effect = def.Resolve(null);
                     // TODO: bind effect.end as callable to args[2]
                     effect.Start(systems, e, null);
@@ -32,7 +36,7 @@ public sealed class TriggerEffect(IServiceFactory services) : BuiltIn("", new("e
                 else if (args[1].Matches(out Location loc)
                     && systems.Get<DungeonSystem>().TryGetTileAt(loc.FloorId, loc.Position, out var tile))
                 {
-                    var def = new EffectDef(stub.Name, stub.Arguments, source: tile);
+                    var def = new EffectDef(stub.Name, stub.Arguments, chance: chance, duration: duration, canStack: canStack, source: tile);
                     var effect = def.Resolve(null);
                     // TODO: bind effect.end as callable to args[2]
                     effect.Start(systems, tile, null);
